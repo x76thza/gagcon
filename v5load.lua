5fefb1e619e3dc2282ef67e322e6158b03bd3154
x76thza <55824836+x76thza@users.noreply.github.com>
2025-08-24 09:42:47 +0700

    Update v5load.lua

--- v5load.lua
+++ v5load.lua
@@ -4,46 +4,80 @@ task.spawn(function()
 end)
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 getgenv().Key = "7e920e1102740422f0ec7d6d"
-getgenv().SettingFarm ={
-    ["Hide UI"] = false,
-    ["Reset Teleport"] = {
-        ["Enabled"] = true,
-        ["Delay Reset"] = 4,
-        ["Item Dont Reset"] = {
-            ["Fruit"] = {
-                ["Enabled"] = true,
-                ["All Fruit"] = false, 
-                ["Select Fruit"] = {
+    getgenv().SettingFarm ={
+        ["Hide UI"] = false,
+        ["Reset Teleport"] = {
+            ["Enabled"] = true,
+            ["Delay Reset"] = 3,
+            ["Item Dont Reset"] = {
+                ["Fruit"] = {
                     ["Enabled"] = true,
-                    ["Fruit"] = {"Dough-Dough","Magma-Magma" },
+                    ["All Fruit"] = true, 
+                    ["Select Fruit"] = {
+                        ["Enabled"] = false,
+                        ["Fruit"] = {},
+                    },
                 },
             },
         },
-    },
-    ["Get Items"] = {
-        ["Godhuman"] =  true,
-        ["Mirror Fractal"] = true,
-        ["Cursed Dual Katana"] = true,
-    },
-    ["Select Hop"] = { -- 70% will have it
-        ["Hop Find Rip Indra Get Valkyrie Helm or Get Tushita"] = true, 
-        ["Hop Find Dough King Get Mirror Fractal"] = false,
-        ["Hop Find Raids Castle [CDK]"] = true,
-        ["Hop Find Cake Queen [CDK]"] = true,
-        ["Hop Find Soul Reaper [CDK]"] = true,
-    },
-    ["Buy Haki"] = {
-        ["Enhancement"] = true,
-        ["Skyjump"] = true,
-        ["Flash Step"] = true,
-        ["Observation"] = true,
-    },
-    ["Lock Fruit"] = {"Kitsune-Kitsune", "Dragon-Dragon", "Gas-Gas", "Yeti-Yeti", "T-Rex-T-Rex", "Mammoth-Mammoth",},
-    ["Webhook"] = {
-        ["Enabled"] = false,
-        ["WebhookUrl"] = "",
+        ["White Screen"] = false,
+        ["Lock Fps"] = {
+            ["Enabled"] = false,
+            ["FPS"] = 20,
+        },
+        ["Get Items"] = {
+            ["Saber"] = true,
+            ["Godhuman"] =  true,
+            ["Skull Guitar"] = true,
+            ["Mirror Fractal"] = true,
+            ["Cursed Dual Katana"] = true,
+            ["Upgrade Race V2-V3"] = true,
+            ["Auto Pull Lever"] = true,
+            ["Shark Anchor"] = true, --- if have cdk,sg,godhuman
+        },
+        ["Get Rare Items"] = {
+            ["Rengoku"] = true,
+            ["Dragon Trident"] = true, 
+            ["Pole (1st Form)"] = true,
+            ["Gravity Blade"]  = true,
+        },
+        ["Farm Fragments"] = {
+            ["Enabled"]  = false,
+            ["Fragment"] = 50000,
+        },
+        ["Auto Chat"] = {
+            ["Enabled"] = false,
+            ["Text"] = "",
+        },
+        ["Auto Summon Rip Indra"] = true, --- auto buy haki and craft haki legendary 
+        ["Select Hop"] = { -- 70% will have it
+            ["Hop Server If Have Player Near"] = false, 
+            ["Hop Find Rip Indra Get Valkyrie Helm or Get Tushita"] = true, 
+            ["Hop Find Dough King Get Mirror Fractal"] = false,
+            ["Hop Find Raids Castle [CDK]"] = true,
+            ["Hop Find Cake Queen [CDK]"] = true,
+            ["Hop Find Soul Reaper [CDK]"] = true,
+            ["Hop Find Darkbeard [SG]"] = true,
+            ["Hop Find Mirage [ Pull Lever ]"] = false,
+        },
+        ["Farm Mastery"] = {
+            ["Melee"] = true,
+            ["Sword"] = true,
+        },
+        ["Buy Haki"] = {
+            ["Enhancement"] = false,
+            ["Skyjump"] = true,
+            ["Flash Step"] = true,
+            ["Observation"] = true,
+        },
+        ["Sniper Fruit Shop"] = {
+            ["Enabled"] = true, -- Auto Buy Fruit in Shop Mirage and Normal
+            ["Fruit"] = {"Leopard-Leopard","Kitsune-Kitsune","Dragon-Dragon","Yeti-Yeti","Gas-Gas"},
+        },
+        ["Lock Fruit"] = {},
+        ["Webhook"] = {
+            ["Enabled"] = false,
+            ["WebhookUrl"] = "",
+        }
     }
-}
-
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaCat-kaitunBF.lua"))()
